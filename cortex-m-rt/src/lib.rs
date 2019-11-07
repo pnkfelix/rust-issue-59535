b154faf2c9ffb645d4cb7cdde85db3c26c4f06c4
Felix S. Klock II <pnkfelix@pnkfx.org>
2019-11-07 22:00:55 +0100

    more-reduction

--- cortex-m-rt/src/lib.rs
+++ cortex-m-rt/src/lib.rs
@@ -9,33 +9,13 @@ pub static __RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
 
 #[no_mangle]
 pub unsafe extern "C" fn Reset() -> ! {
-    extern "C" {
-
-        // These symbols come from `link.x`
-        static mut __sbss: u32;
-        static mut __ebss: u32;
-
-        static mut __sdata: u32;
-        static mut __edata: u32;
-        static __sidata: u32;
-
-    }
-
-    extern "Rust" {
-        // This symbol will be provided by the user via `#[entry]`
-        fn main() -> !;
-
-        // This symbol will be provided by the user via `#[pre_init]`
-    }
-
+    extern "Rust" { fn main() -> !; }
     main()
 }
 
-#[allow(unused_variables)]
-#[doc(hidden)]
 #[link_section = ".HardFault.default"]
 #[no_mangle]
-pub unsafe extern "C" fn HardFault_(ef: &ExceptionFrame) -> ! {
+pub unsafe extern "C" fn HardFault_(_: &ExceptionFrame) -> ! {
     loop {     }
 }
 
@@ -47,94 +27,33 @@ pub unsafe extern "C" fn DefaultHandler_() -> ! {
 #[no_mangle]
 pub unsafe extern "C" fn DefaultPreInit() {}
 
-/* Exceptions */
-#[doc(hidden)]
-pub enum Exception {
-}
-
-extern "C" {
-    fn NonMaskableInt();
-
-    fn HardFaultTrampoline();
-
-    #[cfg(not(armv6m))]
-    fn MemoryManagement();
-
-    #[cfg(not(armv6m))]
-    fn BusFault();
-
-    #[cfg(not(armv6m))]
-    fn UsageFault();
-
-    #[cfg(armv8m)]
-    fn SecureFault();
-
-    fn SVCall();
-
-    #[cfg(not(armv6m))]
-    fn DebugMonitor();
-
-    fn PendSV();
-
-    fn SysTick();
-}
-
-#[doc(hidden)]
+#[allow(dead_code)]
 pub union Vector {
-    handler: unsafe extern "C" fn(),
     reserved: usize,
 }
 
 #[link_section = ".vector_table.exceptions"]
 #[no_mangle]
 pub static __EXCEPTIONS: [Vector; 14] = [
-    Vector {
-        handler: NonMaskableInt,
-    },
-    Vector { handler: HardFaultTrampoline },
-    #[cfg(not(armv6m))]
-    Vector {
-        handler: MemoryManagement,
-    },
-    #[cfg(armv6m)]
-    Vector { reserved: 0 },
-    #[cfg(not(armv6m))]
-    Vector { handler: BusFault },
-    #[cfg(armv6m)]
-    Vector { reserved: 0 },
-    #[cfg(not(armv6m))]
-    Vector {
-        handler: UsageFault,
-    },
-    #[cfg(armv6m)]
-    Vector { reserved: 0 },
-    #[cfg(armv8m)]
-    Vector {
-        handler: SecureFault,
-    },
-    #[cfg(not(armv8m))]
     Vector { reserved: 0 },
     Vector { reserved: 0 },
     Vector { reserved: 0 },
     Vector { reserved: 0 },
-    Vector { handler: SVCall },
-    #[cfg(not(armv6m))]
-    Vector {
-        handler: DebugMonitor,
-    },
-    #[cfg(armv6m)]
     Vector { reserved: 0 },
     Vector { reserved: 0 },
-    Vector { handler: PendSV },
-    Vector { handler: SysTick },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
 ];
 
 #[link_section = ".vector_table.interrupts"]
 #[no_mangle]
 pub static __INTERRUPTS: [unsafe extern "C" fn(); 32] = [{
-    extern "C" {
-        fn DefaultHandler();
-    }
-
+    extern "C" { fn DefaultHandler(); }
     DefaultHandler
 }; 32];
