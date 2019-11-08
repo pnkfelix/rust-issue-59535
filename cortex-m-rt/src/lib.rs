93c50a58ab44bf5ef582229917aa20eb7edc3a42
Felix S. Klock II <pnkfelix@pnkfx.org>
2019-11-08 11:05:39 +0100

    reduce-more

--- cortex-m-rt/src/lib.rs
+++ cortex-m-rt/src/lib.rs
@@ -1,59 +1,13 @@
 #![no_std]
 
-#[repr(C)]
-pub struct ExceptionFrame { }
-
 #[link_section = ".vector_table.reset_vector"]
-#[no_mangle]
-pub static __RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
+#[no_mangle] pub static __RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
 
-#[no_mangle]
-pub unsafe extern "C" fn Reset() -> ! {
-    extern "Rust" { fn main() -> !; }
-    main()
-}
+extern "Rust" { fn main() -> !; }
 
-#[link_section = ".HardFault.default"]
-#[no_mangle]
-pub unsafe extern "C" fn HardFault_(_: &ExceptionFrame) -> ! {
-    loop {     }
-}
+#[no_mangle] pub unsafe extern "C" fn Reset() -> ! { main() }
 
-#[no_mangle]
-pub unsafe extern "C" fn DefaultHandler_() -> ! {
-    loop {    }
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn DefaultPreInit() {}
-
-#[allow(dead_code)]
-pub union Vector {
-    reserved: usize,
-}
+#[no_mangle] pub unsafe extern "C" fn DefaultHandler_() -> ! { loop { } }
 
 #[link_section = ".vector_table.exceptions"]
-#[no_mangle]
-pub static __EXCEPTIONS: [Vector; 14] = [
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-    Vector { reserved: 0 },
-];
-
-#[link_section = ".vector_table.interrupts"]
-#[no_mangle]
-pub static __INTERRUPTS: [unsafe extern "C" fn(); 32] = [{
-    extern "C" { fn DefaultHandler(); }
-    DefaultHandler
-}; 32];
+#[no_mangle] pub static __EXCEPTIONS: [usize; 14] = [0; 14];
